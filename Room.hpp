1027fe937ea5728fd6fd74e0495e0fc8bae4ea65
raymondlyy <raymondlyuan@gmail.com>
2021-02-22 17:18:11 -0800

    Implement getExit

--- Room.hpp
+++ Room.hpp
@@ -16,15 +16,17 @@ public:
     virtual ~Room() = default;
 
     /*
-    interact(): this method happens when 
+    interact(): this method is called whenever the user wants to interact with the room. Should be called during normal traversal
+    args: none
+    outputs: none
     */
     void interact(){
         std::cout << "";
     };
 
     /*
-    addExit: this method adds an exit to the room 
-    args: the room to be added
+    addExit: this method adds an exit to the room. It automatically assigns it a numerical label based on the 
+    args: r (the room to be added)
     outputs: none
     */
     void addExit(Room r){
@@ -33,13 +35,26 @@ public:
     }
 
     /*
-    addExit: same as above
+    addExit: same as above except it adds a string label for the exit. Use if you want to label exits
     args: r (the room to be added), l (the label for the exit)
     outputs: none
     */
     void addExit(Room r, std::string l){
         exits.push_back(r);
         exitLabels.push_back(l);
+    }
+
+    /*
+    getExit: takes in an input label. Returns the matching room if one exists, prints an error if otherwise
+    args: input (the label for the room we want to search for)
+    outputs: the desired room. Returns nullptr if nothing is found
+    */
+    Room* getExit(std::string input){
+        for (int i = 0; i < exitLabels.size(); ++i){
+            if (exitLabels.at(i) == input) return exits.at(i);
+        }
+        std::cout << "Invalid exit. Please try again.\n";
+        return nullptr;
     }
 
     /*
