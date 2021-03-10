9fef2c4f170a802de176bb0c358ea64d6ea141cc
PurebreadDragon <77031974+PurebreadDragon@users.noreply.github.com>
2021-03-09 22:07:31 -0800

    Removed small bug

--- main.cpp
+++ main.cpp
@@ -126,8 +126,6 @@ int main() {
     // arena->addEnemy(ratticus);
     // arena->addEnemy(growslime);
     arena->addEnemy(fairy);
-    GrowSlime* growslime = new GrowSlime();
-    //ShieldSkeleton* shieldSkelly = new ShieldSkeleton();
     arena->addEnemy(skelly);
     arena->addEnemy(shieldSkelly);
     arena->linkPlayer(player);
